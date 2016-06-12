9fdef41df99ff4dffe3ea82e14e650ef850b435a
Ricato <ricatoct@gmail.com>
2016-06-12 15:31:53 -0600

    AVANCE PROBLEMA1 20160612

---
+++ problema1/plot/plot1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FMT_BEGIN=201102060000
+FMT_END=201102060200
+FMT_X_SHOW=%H:%M
+
+
+graficar()
+{
+gnuplot << EOF  2> error.log
+set xdata time
+set timefmt "%Y%m%d%H%M"
+set xrange ["$FMT_BEGIN" : "$FMT_END"]
+set format x "$FMT_X_SHOW"
+set terminal png
+set output 'fig1.png'
+plot "graf-0.dat" using 1:2 with lines title "sensor1","graf-0.dat" using 1:3 with linespoints title "sensor2"
+EOF
+}
+
+graficar
+
