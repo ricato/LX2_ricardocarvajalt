1097bab219da10385cbc3aa963f7121b988ec2b7
Ricato <ricatoct@gmail.com>
2016-06-12 16:45:42 -0600

    Problema1:script plot1 ya grafica :-)

--- problema1/plot/plot1.sh
+++ problema1/plot/plot1.sh
@@ -9,12 +9,12 @@ graficar()
 {
 gnuplot << EOF  2> error.log
 set xdata time
-set timefmt "%Y%m%d%H%M"
-set xrange ["$FMT_BEGIN" : "$FMT_END"]
+set timefmt "%Y%m%d %H%M"
+set xrange ["$FMT_BEGIN":"$FMT_END"]
 set format x "$FMT_X_SHOW"
 set terminal png
 set output 'fig1.png'
-plot "graf-0.dat" using 1:2 with lines title "sensor1","graf-0.dat" using 1:3 with linespoints title "sensor2"
+plot "graf-0.dat" using 1:3 with lines title "sensor1","graf-0.dat" using 1:4 with linespoints title "sensor2"
 EOF
 }
 
