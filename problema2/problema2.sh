675377b8a763e7474f8982f4142dd7228a6771ce
Ricardo <ricatoct@gmail.com>
2016-06-23 22:41:07 -0600

    Avance problema2

--- problema2/problema2.sh
+++ problema2/problema2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATA=/home/ricato/LX2_ricardocarvajalt/problema2/Datos
+DATA=/home/ricardo/LX2_ricato/problema2/Datos
 OUT_DATA=$DATA/archivos_csv
 GRAF_DATA=$DATA/datos_graf
 FULL_DATA=$DATA/full_datos
@@ -9,10 +9,11 @@ mkdir $DATA/archivos_csv
 mkdir $GRAF_DATA
 mkdir $FULL_DATA
 
-m=0
+m=1
 
-for i in  `find $DATA -name '*.xls' `
+for i in  `find $DATA -name '*.xls' |sort `
 do
+	
         echo "Procesando archivo $i"
 
         xls2csv $i > $OUT_DATA/dataTmp-$m.csv
@@ -21,9 +22,9 @@ do
         let m=m+1
 done 2> error1.log
 
-m=0
+m=1
 
-for e in `find $OUT_DATA -name "*.csv"`
+for e in `find $OUT_DATA -name "*.csv" | sort`
 do
         echo "Dando formato de datos para graficar el archivo $e"
         cat $e | awk -F "\",\"" '{print $1 " " $2 " " $3}' | sed '1,$ s/"//g'| sed '1,$ s/,//g'| sed '1d' > $GRAF_DATA/graf-$m.dat
@@ -33,18 +34,30 @@ done 2> error2.log
 
 ### Condicional
 
-if [-a $FULL_DATA/full.dat ]
+if [ -a $FULL_DATA/full.dat ]
 then
         rm $FULL_DATA/full.dat
         echo "Archivo full.dat borrado"
 fi 2> errorIf.log
 
+#a=1
+#for k in `find $GRAF_DATA -name "*.dat" | sort `
+#do
+#	if [$a < 3]
+#	then
+#        	sed '3!d' $k >> $FULL_DATA/full.dat
+#        	echo "Procesando archivo $k"
+#		let a=a+1
+#	fi 2> errorIf2.log
+#done 2> error3.log
 
-for k in `find $GRAF_DATA -name "*.dat" `
+
+for k in `find $GRAF_DATA -name "*.dat" | sort `
 do
         sed '3!d' $k >> $FULL_DATA/full.dat
         echo "Procesando archivo $k"
 done 2> error3.log
+
 
 
 #Grafica
@@ -59,7 +72,7 @@ DATA_DONE=$FULL_DATA/full.dat
 graficar()
 {
 gnuplot << EOF 2> error.log
-set title "Consumo Electrico"
+set title "Consumo Electrico primeros 3 meses 2012 "
 #set timefmt "%Y%m%d %H%M"
 #set xrange ["$FMT_BEGIN":"$FMT_END"]
 #set format x "$FMT_X_SHOW"
